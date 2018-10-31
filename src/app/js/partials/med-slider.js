4cab9ccf89ddef99becf9ce238b403b6790ba9d1
Yauheni Humanau <idracat@gmail.com>
2018-10-31 11:54:09 +0300

    med-slider
    done

---
+++ src/app/js/partials/med-slider.js
@@ -0,0 +1,16 @@
+$('.slider-block__med-slider-items').slick({
+    dots: false,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    arrows: true,
+    centerMode: false,
+    responsive: [
+        {
+            breakpoint: 1201,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
